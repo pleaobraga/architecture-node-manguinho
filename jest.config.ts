c1206a4a9dffb3370ca6708ed1480ae53a5de78a
pleaobraga <pleao.braga@gmail.com>
2021-07-06 10:33:12 -0300

    refactor:  move success http response to a helper method

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,7 @@
 export default {
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
-  preset: 'ts-jest',
-  testMatch: ['**/*.spec.ts']
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  preset: "ts-jest",
+  testMatch: ["**/*.spec.ts"],
 }
