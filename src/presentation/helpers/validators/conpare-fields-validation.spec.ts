688384f9dfaec10d465bff4fe1a3b04a216652d3
pleaobraga <pleao.braga@gmail.com>
2021-07-10 15:04:58 -0300

    test: create RequiredFieldValidation tests

---
+++ src/presentation/helpers/validators/conpare-fields-validation.spec.ts
@@ -0,0 +1,22 @@
+import { InvalidParamError } from "../../errors"
+import { CompareFieldsValidation } from "./compare-fields-validation"
+
+describe("CompareFields Validation", () => {
+  it("Should return A MissingParam error if validation fails", () => {
+    const sut = new CompareFieldsValidation("field", "fieldToCompare")
+    const error = sut.validate({
+      field: "any_value",
+      fieldToCompare: "wrong_value",
+    })
+    expect(error).toEqual(new InvalidParamError("fieldToCompare"))
+  })
+
+  it("Should not return if validation succeeds", () => {
+    const sut = new CompareFieldsValidation("field", "fieldToCompare")
+    const error = sut.validate({
+      field: "any_value",
+      fieldToCompare: "any_value",
+    })
+    expect(error).toBeFalsy()
+  })
+})
