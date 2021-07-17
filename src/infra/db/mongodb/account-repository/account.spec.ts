abba50aca4b3ca4a67af1f5fa46b6fe767e03dea
pleaobraga <pleao.braga@gmail.com>
2021-07-17 14:00:06 -0300

    test: ensure Account return an account on loadByEmail success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,5 +1,9 @@
+import { Collection } from "mongodb"
+
 import { MongoHelper } from "../helpers/mongo-helper"
 import { AccountMongoRepository } from "./account"
+
+let AccountCollection: Collection
 
 describe("Account Mongo Repository", () => {
   beforeAll(async () => {
@@ -11,7 +15,7 @@ describe("Account Mongo Repository", () => {
   })
 
   beforeEach(async () => {
-    const AccountCollection = await MongoHelper.getCollection("accounts")
+    AccountCollection = await MongoHelper.getCollection("accounts")
     await AccountCollection.deleteMany({})
   })
 
@@ -19,7 +23,7 @@ describe("Account Mongo Repository", () => {
     return new AccountMongoRepository()
   }
 
-  it("Should return an account on success", async () => {
+  it("Should return an account on add success", async () => {
     const sut = makeSut()
     const account = await sut.add({
       name: "any_name",
@@ -32,5 +36,29 @@ describe("Account Mongo Repository", () => {
     expect(account.name).toBe("any_name")
     expect(account.email).toBe("any_email@email.com")
     expect(account.password).toBe("any_password")
+  })
+
+  it("Should return an account on loadByEmail success", async () => {
+    const sut = makeSut()
+
+    await AccountCollection.insertOne({
+      name: "any_name",
+      email: "any_email@email.com",
+      password: "any_password",
+    })
+
+    const account = await sut.loadByEmail("any_email@email.com")
+
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe("any_name")
+    expect(account.email).toBe("any_email@email.com")
+    expect(account.password).toBe("any_password")
+  })
+
+  it("Should return null if loadByEmail fails", async () => {
+    const sut = makeSut()
+    const account = await sut.loadByEmail("any_email@email.com")
+    expect(account).toBeFalsy()
   })
 })
