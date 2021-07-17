33324d95f8f4e7f72c6c3190bef75156bf205a2b
pleaobraga <pleao.braga@gmail.com>
2021-07-17 14:09:13 -0300

    test: ensure Account updates the account accessToken on updateAccessToken success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,9 +1,10 @@
+/* eslint-disable no-underscore-dangle */
 import { Collection } from "mongodb"
 
 import { MongoHelper } from "../helpers/mongo-helper"
 import { AccountMongoRepository } from "./account"
 
-let AccountCollection: Collection
+let accountCollection: Collection
 
 describe("Account Mongo Repository", () => {
   beforeAll(async () => {
@@ -15,8 +16,8 @@ describe("Account Mongo Repository", () => {
   })
 
   beforeEach(async () => {
-    AccountCollection = await MongoHelper.getCollection("accounts")
-    await AccountCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection("accounts")
+    await accountCollection.deleteMany({})
   })
 
   const makeSut = (): AccountMongoRepository => {
@@ -41,7 +42,7 @@ describe("Account Mongo Repository", () => {
   it("Should return an account on loadByEmail success", async () => {
     const sut = makeSut()
 
-    await AccountCollection.insertOne({
+    await accountCollection.insertOne({
       name: "any_name",
       email: "any_email@email.com",
       password: "any_password",
@@ -60,5 +61,25 @@ describe("Account Mongo Repository", () => {
     const sut = makeSut()
     const account = await sut.loadByEmail("any_email@email.com")
     expect(account).toBeFalsy()
+  })
+
+  it("Should update the account accessToken on updateAccessToken success", async () => {
+    const sut = makeSut()
+    const res = await accountCollection.insertOne({
+      name: "any_name",
+      email: "any_email@email.com",
+      password: "any_password",
+    })
+
+    const fakeAccount = res.ops[0]
+
+    expect(fakeAccount.accessToken).toBeFalsy()
+
+    await sut.updateAccessToken(fakeAccount._id, "any_token")
+
+    const account = await accountCollection.findOne({ _id: fakeAccount._id })
+
+    expect(account).toBeTruthy()
+    expect(account.accessToken).toBe("any_token")
   })
 })
