dca4d1f8b5213739a8bcfa7a207a262102ae070b
pleaobraga <pleao.braga@gmail.com>
2021-07-17 13:35:58 -0300

    test: ensure JwtAdapter calls  sign with correct values

---
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken"
+
+import { JwtAdapter } from "./jwt-adapter"
+
+// jest.mock("jwt", () => ({
+//   async sign(): Promise<string> {
+//     return new Promise((resolve) => resolve("hash"))
+//   },
+// }))
+
+const makeSut = (): JwtAdapter => {
+  return new JwtAdapter("secret")
+}
+
+describe("JWT Adapter", () => {
+  it("Should call sign with correct values", async () => {
+    const sut = makeSut()
+    const signSpy = jest.spyOn(jwt, "sign")
+    await sut.encrypt("any_id")
+
+    expect(signSpy).toHaveBeenCalledWith({ id: "any_id" }, "secret")
+  })
+})
