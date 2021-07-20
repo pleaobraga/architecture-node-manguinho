5a053e3ef9dc9e20e85c739309e2c7adc47c328e
pleaobraga <pleao.braga@gmail.com>
2021-07-20 06:41:16 -0300

    refactor: change routes to use filesystem instead fast-global

--- src/data/protocols/db/account/load-account-by-email-repository.ts
+++ src/data/protocols/db/account/load-account-by-email-repository.ts
@@ -1,4 +1,4 @@
-import { AccountModel } from "../../../domain/models/accounts"
+import { AccountModel } from "../../../../domain/models/accounts"
 
 export interface LoadAccountByEmailRepository {
   loadByEmail(email: string): Promise<AccountModel>
